6797d0c49193e42e53d522b6f0f77ca03620ddda
RushilShah01 <121447143+RushilShah01@users.noreply.github.com>
2024-06-02 13:26:14 +0530

    Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
       <p className='git'>Get In Touch</p>
-      <h1 className='con'>Contact Me</h1>
+      <h1 className='con'>Connect</h1>
       <div className='contact' id='contact'>
         <a href="https://www.linkedin.com/in/rushil-shah-6934b1195/" class="social-icon" target="_blank"><i class="fab fa-linkedin-in"></i></a>
         <a href="https://github.com/RushilShah01" class="social-icon" target="_blank"><i class="fab fa-github"></i></a>
